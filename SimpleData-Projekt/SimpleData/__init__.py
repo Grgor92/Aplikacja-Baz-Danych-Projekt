a5ae1e5006ae2d7c36c68e5be959ef7c3a1b85bd
Grzegorz Bobak <grzegorzbobak00@gmail.com>
2023-04-13 22:48:33 +0200

    Poprawa bezpieczenstwa

--- SimpleData-Projekt/SimpleData/__init__.py
+++ SimpleData-Projekt/SimpleData/__init__.py
@@ -2,12 +2,24 @@
 #Import biblioteki
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, session
+from datetime import timedelta
+import os
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "e761d6362a5b7811a2b1a7e227e39900"
+# generowanie klucza losowego
+SECRET_KEY = os.urandom(32)
+
+# ustawienie klucza w konfiguracji aplikacji
+app.config['SECRET_KEY'] = SECRET_KEY
+
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 @app.before_first_request
 def init_session():
     session['user'] = None
+    session.permanent = False
 
 #Łączenie z bazą danych
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/sd_baza'
