aba7e563ec793afd013f95b11be4225d68600e1b
Grzegorz Bobak <grzegorzbobak00@gmail.com>
2023-04-20 16:46:15 +0200

    zmiana foramtu plikow

--- SimpleData-Projekt/SimpleData/forms.py
+++ SimpleData-Projekt/SimpleData/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField #importujemy odpowiednie elemnety aby móc sprawdziæ poprawnoœc formularza
+ï»¿from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField #importujemy odpowiednie elemnety aby mÃ³c sprawdziÄ‡ poprawnoÅ›c formularza
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class RegistrationForm(FlaskForm):  #tworzymy klasê o odppowiedniej nazwie
-    Nazwa = StringField('Nazwa', validators=[DataRequired(), Length(min=6, max=20)])    #tworzymy pola i definjujemy typ zmiennej, oraz dodajemy poprawnoœci jakie ma zawieraæ pole
+class RegistrationForm(FlaskForm):  #tworzymy klasÄ™ o odppowiedniej nazwie
+    Nazwa = StringField('Nazwa', validators=[DataRequired(), Length(min=6, max=20)])    #tworzymy pola i definjujemy typ zmiennej, oraz dodajemy poprawnoï¿½ci jakie ma zawiera pole
     email = StringField('Email', validators=[DataRequired(), Email()])
     haslo = PasswordField('Haslo', validators=[DataRequired(),Length(min=6, max=32)])
     haslo2 = PasswordField('Potwierdz Haslo', validators=[DataRequired(),EqualTo('haslo'),Length(min=6, max=32)])
